65fb53378e4abb2e76c8e4a3a228060f61046ce4
Mathieu VATEL <mathieu@julius.fr>
2015-10-07 14:32:14 +0200

    [FIX] the attribute "data" was writen "datas"

--- report_aeroo/report.py
+++ report_aeroo/report.py
@@ -35,9 +35,9 @@ class Report(models.Model):
             new_report_name = name
         return new_report_name
 
-    def get_action(self, cr, uid, ids, report_name, datas=None, context=None):
+    def get_action(self, cr, uid, ids, report_name, data=None, context=None):
         return super(Report, self).get_action(
             cr, uid, ids,
-            self._get_replacing_report(cr, report_name), datas=datas, context=context)
+            self._get_replacing_report(cr, report_name), data=data, context=context)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
